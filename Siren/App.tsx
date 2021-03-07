f8b050c3c9465c7cf2d49f23a6ed14290278b432
Aamir Tahir <saamirt@gmail.com>
2021-03-06 22:48:29 -0500

    push notifications

--- Siren/App.tsx
+++ Siren/App.tsx
@@ -1,9 +1,12 @@
 import { Audio, AVPlaybackStatus } from 'expo-av';
-import React, { useEffect } from 'react';
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, Text, TouchableOpacity, View, SafeAreaView, Platform, StatusBar } from 'react-native';
 import * as Permissions from 'expo-permissions';
 import { usePermissions } from 'expo-permissions';
 import * as FileSystem from 'expo-file-system';
+import Constants from 'expo-constants';
+import * as Notifications from 'expo-notifications';
+
 import {
   RecordingOptions,
   RECORDING_OPTION_ANDROID_AUDIO_ENCODER_AAC,
@@ -11,7 +14,6 @@ import {
   RECORDING_OPTION_IOS_AUDIO_QUALITY_MAX,
   RECORDING_OPTION_IOS_OUTPUT_FORMAT_MPEG4AAC
 } from 'expo-av/build/Audio';
-import { FileSystemUploadType } from 'expo-file-system';
 
 export const RECORDING_OPTIONS_PRESET_HIGH_QUALITY: RecordingOptions = {
   android: {
@@ -29,12 +31,69 @@ export const RECORDING_OPTIONS_PRESET_HIGH_QUALITY: RecordingOptions = {
   },
 };
 
+Notifications.setNotificationHandler({
+	handleNotification: async () => ({
+		shouldShowAlert: true,
+		shouldPlaySound: true,
+		shouldSetBadge: false
+	})
+});
+
+async function sendPushNotification(message: object) {
+  console.log(message);
+	await fetch('https://exp.host/--/api/v2/push/send', {
+		method: 'POST',
+		headers: {
+			Accept: 'application/json',
+			'Accept-encoding': 'gzip, deflate',
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify(message)
+	});
+}
+
+async function registerForPushNotificationsAsync() {
+	let token;
+	if (Constants.isDevice) {
+		const { status: existingStatus } = await Notifications.getPermissionsAsync();
+		let finalStatus = existingStatus;
+		if (existingStatus !== 'granted') {
+			const { status } = await Notifications.requestPermissionsAsync();
+			finalStatus = status;
+		}
+		if (finalStatus !== 'granted') {
+			alert('Failed to get push token for push notification!');
+			return;
+		}
+		token = (await Notifications.getExpoPushTokenAsync()).data;
+		console.log(token);
+	} else {
+		alert('Must use physical device for Push Notifications');
+	}
+
+	if (Platform.OS === 'android') {
+		Notifications.setNotificationChannelAsync('default', {
+			name: 'default',
+			importance: Notifications.AndroidImportance.MAX,
+			vibrationPattern: [ 0, 250, 250, 250 ],
+			lightColor: '#FF231F7C'
+		});
+	}
+
+	return token;
+}
+
 export default function App() {
+	// Notification
+	const [ expoPushToken, setExpoPushToken ] = useState('');
   const [recordingPermission, askForPermission] = usePermissions(Permissions.AUDIO_RECORDING, { ask: true });
+
   useEffect(() => {
     askForPermission()
   });
-
+	useEffect(() => {
+		registerForPushNotificationsAsync().then((token) => setExpoPushToken(token));
+	}, []);
 
   const [recording, setRecording] = React.useState<Audio.Recording | undefined>(undefined);
   const [recordingBuffer, bufferAudio] = React.useState<Audio.Recording | undefined>(undefined);
@@ -53,23 +112,33 @@ export default function App() {
 
   async function classifyAudio() {
 
-    if (recordingBuffer && recordingBuffer != null) {
+    if (recordingBuffer) {
       const uri = recordingBuffer.getURI() ?? '';
       console.log("send audio: " + recordingBuffer);
       console.log("audio uri: " + uri);
 
-      FileSystem.uploadAsync(URL, uri,
+      const response = await FileSystem.uploadAsync(URL, uri,
         {
           headers: { 'Content-Type': 'multipart/form-data' },
           httpMethod: 'POST',
-          uploadType: FileSystemUploadType.MULTIPART,
+          uploadType: FileSystem.FileSystemUploadType.MULTIPART,
           fieldName: 'file',
           mimeType: 'audio/mp4'
         })
         .then(response => console.log("response: " + JSON.stringify(response)))
-        .then(data => console.log("data: " + data))
+        .then(data => data)
         .catch(error => console.log(error));
 
+        console.log("Data:");
+        console.log(response);
+        
+        // await sendPushNotification({
+        //   to: expoPushToken,
+        //   sound: 'default',
+        //   title: response.predicted_class,
+        //   body: 'Predicted class is ' + response.predicted_class,
+        //   autoDismiss: true
+        // });
     }
   }
 
@@ -154,50 +223,77 @@ export default function App() {
   }
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>
-        This is an Audio siren app. Tap record to begin recording and play to play what was recorded.
-      </Text>
-      <View style={styles.buttonLayout}>
-        <TouchableOpacity style={styles.button} onPress={recording ? stopRecording : beginRecording} activeOpacity={.5}>
-          <Text style={styles.buttonText}>{recording ? 'Stop Recording' : 'Begin Recording'}</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.button} onPress={onPlayPausedPressed} activeOpacity={.5}>
-          <Text style={styles.buttonText}>{isPlaying ? 'Pause' : 'Play'}</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.button} onPress={classifyAudio} activeOpacity={0.5}>
-          <Text style={styles.buttonText}>Send Audio</Text>
-        </TouchableOpacity>
-      </View>
-    </View>
+		<SafeAreaView style={styles.container}>
+			<View style={styles.buttonLayout}>
+				<TouchableOpacity
+					style={styles.button}
+					onPress={recording ? stopRecording : beginRecording}
+					activeOpacity={0.5}
+				>
+					<Text style={styles.buttonText}>{recording ? 'Stop Recording' : 'Begin Recording'}</Text>
+				</TouchableOpacity>
+				<TouchableOpacity style={styles.button} onPress={onPlayPausedPressed} activeOpacity={0.5}>
+					<Text style={styles.buttonText}>{isPlaying ? 'Pause' : 'Play'}</Text>
+				</TouchableOpacity>
+				<TouchableOpacity style={styles.button} onPress={classifyAudio} activeOpacity={0.5}>
+					<Text style={styles.buttonText}>Send Audio</Text>
+				</TouchableOpacity>
+				<TouchableOpacity
+					style={styles.button}
+					onPress={async () => {
+						await sendPushNotification({
+							to: expoPushToken,
+							sound: 'default',
+							title: 'Original Title',
+							body: 'And here is the body!',
+							data: { someData: 'goes here' },
+							autoDismiss: true
+						});
+					}}
+					activeOpacity={0.5}
+				>
+					<Text style={styles.buttonText}>Test Notification</Text>
+				</TouchableOpacity>
+				<Text style={styles.title}>Your expo push token: {expoPushToken}</Text>
+			</View>
+		</SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    marginHorizontal: 16,
-  },
-  title: {
-    color: '#888',
-    fontSize: 18,
-    marginVertical: 15,
-    textAlign: 'center',
-  },
-  button: {
-    backgroundColor: "blue",
-    padding: 20,
-    borderRadius: 5,
-  },
-  buttonText: {
-    fontSize: 20,
-    color: '#fff',
-  },
-  buttonLayout: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-  }
+	container: {
+		flex: 1,
+		backgroundColor: '#171717',
+		alignItems: 'center',
+		justifyContent: 'center',
+		paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0
+	},
+	title: {
+		color: '#fff',
+		fontSize: 18,
+		marginVertical: 15,
+		textAlign: 'center'
+	},
+	button: {
+		backgroundColor: '#007AFF',
+		justifyContent: 'center',
+		alignItems: 'center',
+		flexDirection: 'row',
+		borderRadius: 5,
+		padding: 16,
+		height: 50,
+		marginTop: 10
+	},
+	buttonText: {
+		fontSize: 20,
+		color: '#fff',
+		textAlign: 'center'
+	},
+	buttonLayout: {
+		flexDirection: 'column',
+		// justifyContent: 'space-between'
+		marginLeft: 50,
+		marginRight: 50,
+		minWidth: 250
+	}
 });
